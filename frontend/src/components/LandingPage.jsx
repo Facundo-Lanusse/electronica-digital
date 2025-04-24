50f59a705b583af62ddc06bcaa11e1fffc9a436b
biancacomotto <bcomotto@mail.austral.edu.ar>
2025-04-24 11:54:56 -0300

    Frontend Login y Register navtab

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/LandingPage.css';
 
@@ -8,14 +9,50 @@ const LandingPage = () => {
     const handleClick = () => {
         navigate('/home');
     };
+    const [activeTab, setActiveTab] = useState(null);
+    const tabs = [
+        { id: "tab1", label: "Register" },
+        { id: "tab2", label: "Login" }
+    ];
+
 
     return (
-        <div className="landing-page">
-            <div className="overlay">
+        <div className="landing-container">
+            {activeTab === null && (<div className="box-Welcome">
                 <h1>Welcome</h1>
                 <p>Reserva tu lugar</p>
                 <button onClick={handleClick}>Get Started</button>
+            </div>)} {/*//null tab , mi inicio*/}
+            <div className="tab-section">
+                <div className="NavigateOption">
+                    {tabs.map((tab) => (
+                        <button
+                            key={tab.id}
+                            className={`tab-bottom ${
+                                activeTab === tab.id
+                                    ? 'active-tab'
+                                    : activeTab === null
+                                        ? 'neutral-tab'
+                                        : 'inactive-tab'
+                            }`}
+                            onClick={() => setActiveTab(tab.id)}
+                        >
+                            {tab.label}
+                        </button>
+                    ))}
+                </div>
+                {activeTab === "tab1" && (
+                    <div className="box-navigation">
+                        <h2>Register</h2>
+                    </div>
+                )}
+                {activeTab === "tab2" && (
+                    <div className="box-navigation">
+                        <h2>Login</h2>
+                    </div>
+                )}
             </div>
+
         </div>
     );
 };
